091c19ce35e5e81c5aced4e07c3b061dda67c926
Isaiah Mindich <immindich@gmail.com>
2026-03-24 14:20:27 -0500

    Remove support for qwen models

--- models.py
+++ models.py
@@ -1,19 +1,4 @@
 AVAILABLE_MODELS = {
-    "qwen-0.6b": {
-        "name": "qwen3-0.6b",
-        "path": "Qwen/Qwen3-0.6B",
-        "type": "local",
-    },
-    "qwen-4b": {
-        "name": "qwen3-4b-instruct",
-        "path": "Qwen/Qwen3-4B-Instruct-2507",
-        "type": "local",
-    },
-    "qwen-14b": {
-        "name": "qwen3-14b",
-        "path": "Qwen/Qwen3-14B",
-        "type": "local",
-    },
     "gemma-12b": {
         "name": "gemma-3-12b-it",
         "path": "google/gemma-3-12b-it",
